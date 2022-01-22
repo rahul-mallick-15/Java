dc46841cbbd851df02fbb9982a48e38262be2eb4
Rahul Mallick <rahulmallick1115@gmail.com>
2022-01-22 13:43:49 +0530

    Added new.java to master

---
+++ CP_Syllogistek/Jan22.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class TComparator implemnents Comparator<ListNode>{
+    public int compare(ListNode a , ListNode b){
+        if(a.val >= )
+    }
+}
+class Main{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a Number: ");
+        int num = sc.nextInt();
+        System.out.println("Hello "+num );
+    }
+}
