6b3139ab8c7a90b93edeac840940971bd7651b13
Rahul Mallick <rahulmallick1115@gmail.com>
2022-02-09 22:52:29 +0530

    BubbleSort Optimised

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -1,27 +1,38 @@
 import java.io.*;
 class Sort{
-    void bubbleSort(int array[]){
+    /*/ WORST and Average Case Time Complexity: O(n * n)
+    /// BEST Case TIme Complexity             : O(n) (for Sorted Array)    
+    /// Auxiliary Space                       : O(1) (Stable Sorting)
+    /*/ 
+    public static void bubbleSort(int array[]){
         for(int i=0 ; i<array.length-1;++i){
+            boolean swapping = false;
             for(int j= i+1; j<array.length ; ++j){
                 if(array[i] > array[j]){
                     int temp = array[i] ;
                     array[i] = array[j] ;
                     array[j] = temp ;
+                    swapping = true ;
                 }
             }
+            if(!swapping) 
+                break ;
         }
+    }
+}
+class Display{
+    public static void printArray(int array[]){
+        for(int i = 0 ; i < array.length; ++i){
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
     }
 }     
 class Mains{
     public static void main(String args[]){
         int array[] = {5 , 3 ,4 ,2 , 1};
-        for(int element:array){
-            System.out.print(element+ " ");
-        }
-        System.out.println();
-        (new Sort()).bubbleSort(array);
-        for(int element:array){
-            System.out.print(element+ " ");
-        }
+        Display.printArray(array);
+        Sort.bubbleSort(array);
+        Display.printArray(array);
     }
 } 
